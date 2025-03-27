1312593b66850c77a9dc73f250c6312a45697cca
sharath0404 <59417982+sharath0404@users.noreply.github.com>
2025-03-27 16:21:58 +0530

    Update main.tf

--- Test_vpc/main.tf
+++ Test_vpc/main.tf
@@ -1,4 +1,3 @@
-/*
 module "vpc" {
 source = "../modules/vpc_modules"
 }
@@ -6,7 +5,7 @@ source = "../modules/vpc_modules"
 module "aws_new" {
     source = "../modules/ec2_modules"
     vpc_id = module.vpc.my_output
-}*/
+}
 module "s3_create" {
   source = "../modules/s3_modules"
 }
